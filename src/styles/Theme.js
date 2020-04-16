3ba38de127f40e3c96b694b14f78b6a55f1400fb
uijin <justwaves@naver.com>
2020-04-16 21:21:16 +0900

    theme 수정

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -3,24 +3,41 @@ const BORDER_RADIUS = "4px";
 
 export default {
   // size
-  maxWidth: "1920px",
-  cardWidth: "360px",
-  minPageWidth: "360px",
+
+  // width
+  width: {
+    container: "1200px",
+    gutter: "24px",
+    column: "72px",
+    containerPadding: "36px",
+    card: "360px",
+
+    colunms: [
+      "75rem", // 0 column - 1200px
+      "70.5rem", // 1 column - 1128px
+      "34.5rem", // 2 column - 552px
+      "22.5rem", // 3 column - 360px
+      "16.5rem", // 4 column - 264px
+      "16.5rem",
+      "10.5rem", // 6 column - 168px
+      "4.5rem", // 12 column - 72px
+    ],
+  },
 
   // color
   color: {
     bg: "white",
     gray: [
-      "#f8f9fa",
-      "#f1f3f5",
-      "#e9ecef", // background color
-      "#dee2e6",
-      "#ced4da",
-      "#adb5bd",
-      "#868e96",
-      "#495057",
-      "#343a40",
-      "#212529",
+      "#ffffff",
+      "#fafafa",
+      "#f5f5f5",
+      "#e8e8e8",
+      "#d9d9d9",
+      "#bfbfbf",
+      "#8C8C8C",
+      "#595959",
+      "#262626",
+      "#000000",
     ],
     cyan: [
       "#e3fafc",
@@ -34,6 +51,12 @@ export default {
       "#0c8599",
       "#0b7285",
     ],
+    primary: ["#4652DC", "#A6B0FE", "#7F89E3", "#5B63A3", "#383C63"],
+    yellow: "#FFE500",
+    yellowDark: "#ECCE34",
+    green: "#006666",
+    turquoise: "#04CECE",
+    red: "#FF1A40",
   },
 
   // whiteBox

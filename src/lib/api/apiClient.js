836405eba0863b56171fd78a44545cc757211031
uijin <justwaves@naver.com>
2020-05-22 20:45:02 +0900

    cors

--- src/lib/api/apiClient.js
+++ src/lib/api/apiClient.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
 // const host = process.env.REACT_APP_API_HOST;
-const host =
-  process.env.NODE_ENV === 'development'
-    ? '/'
-    : process.env.REACT_APP_API_HOST || '/';
+// const host =
+//   process.env.NODE_ENV === 'development'
+//     ? '/'
+//     : process.env.REACT_APP_API_HOST || '/';
 
-const apiClient = axios.create({
-  baseURL: host,
-  withCredentials: true,
-});
+// const apiClient = axios.create({
+//   baseURL: host,
+//   withCredentials: true,
+// });
 
-// const apiClient = axios.create();
+const apiClient = axios.create();
 
 export default apiClient;
